87c8e51cbd0baeee2fb3f587984c7a1aaf2e83a5
Lauren Laframboise <laurenlaframboise@gmail.com>
2017-03-26 20:01:16 +0000

    add post friends functionality

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/hello", function(req, res){
+var friends = ["tony", "Miranda", "Justin", "Pierre", "Lilly"];
+
+app.get("/", function(req, res){
     res.render("home");
+});
+
+app.get("/friends", function(req, res){
+    res.render("friends", {friends: friends});
+});
+
+app.post("/addfriend", function(req, res){
+    console.log(req.body.newfriend);
+    // res.send("you have reached the post route");
+        var newFriend = req.body.newfriend;
+        friends.push(newFriend);
+        res.redirect("/friends");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
